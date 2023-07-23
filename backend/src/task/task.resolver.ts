36b2208fe04beb164779225da8343c040552ea2d
Yusei Mizutani <yusei4520030401@gmail.com>
2023-07-23 13:57:02 +0900

    バリデーション

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task } from './models/task.model';
 import { Query } from '@nestjs/graphql';
+import { CreateTaskInput } from './dto/createTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -13,11 +14,7 @@ export class TaskResolver {
   }
 
   @Mutation(() => Task)
-  createTask(
-    @Args('name') name: string,
-    @Args('dueDate') dueDate: string,
-    @Args('description', { nullable: true }) description?: string,
-  ): Task {
-    return this.taskService.createTask(name, dueDate, description);
+  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+    return this.taskService.createTask(createTaskInput);
   }
 }
