9a8d635015465bb839dde2cec9dac07b09730742
Yusei Mizutani <yusei4520030401@gmail.com>
2023-07-23 11:55:41 +0900

    リゾルバー

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,4 +1,14 @@
 import { Resolver } from '@nestjs/graphql';
+import { TaskService } from './task.service';
+import { Task } from './models/task.model';
+import { Query } from '@nestjs/graphql';
 
 @Resolver()
-export class TaskResolver {}
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+}
