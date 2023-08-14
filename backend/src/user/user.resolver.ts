fc16e980793b6632132fbc42296320aa7efe8ca5
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-14 20:22:42 +0900

    ユーザーつ一機能

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service';
 import { CreateUserInput } from './dto/createUser.input';
 import { User } from '@prisma/client';
 import { User as UserModel } from './models/user.model';
+import { Query } from '@nestjs/common';
+import { GetUserArgs } from './dto/getUser.args';
 
 @Resolver()
 export class UserResolver {
@@ -13,5 +15,10 @@ export class UserResolver {
     @Args('createUserInput') createUserInput: CreateUserInput,
   ): Promise<User> {
     return await this.userService.createUser(createUserInput);
+  }
+
+  @Query(() => UserModel, { nullable: true })
+  async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return await this.userService.getUser(getUserArgs.email);
   }
 }
